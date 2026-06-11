aae6b79f28170305a4dbbe79a7a6ed1e0395caa2
Anyakin <aninasvi@gmail.com>
2026-06-11 21:18:40 +0300

    tryingfirst

---
+++ solution.js
@@ -0,0 +1,19 @@
+function printNumberTriangle(n) {
+    let result = '';
+    let currentNumber = 1;
+    
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            result += currentNumber;
+            if (j < i) {
+                result += ' ';
+            }
+            currentNumber++;
+        }
+        result += '\n';
+    }
+    
+    return result;
+}
+
+module.exports = { printNumberTriangle };
